4f54ca74e704d7024141bdd2d19c052aceaa63ef
ConnorUsaty <connorusaty@gmail.com>
2024-05-13 22:19:24 -0400

    Refactored code logic + fixed image paths

--- main.py
+++ main.py
@@ -3,58 +3,54 @@ from time import sleep
 
 # Dict of keys and their corresponding actions (i.e. pressing "w" moves the character forward, pressing "a" makes the character attack)
 # Make sure these match the keybinds in the game
-actions = {"walkForward": "w", "walkBackward": "s", "attacking": "a", "mine": "m"}
+actions = {"walkForward": "w", "walkBackward": "s", "attacking": "a"}
 
 
-def navToImage(image, clicks, off_x=0, off_y=0):
-    pos = pt.locateCenterOnScreen(image, confidence=0.7)
-
-    if pos is None:
-        print(f"Could not find {image}")
+def readyToStart():
+    try:
+        pos = pt.locateCenterOnScreen('images/backToGame.png', confidence=0.6)
+    except:
         return False
     
-    pt.moveTo(pos[0] + off_x, pos[1] + off_y, duration=0.1)
-    pt.click(clicks=clicks, interval=0.25)
+    pt.moveTo(pos[0], pos[1], duration=0.2)
+    pt.click(clicks=1, interval=0.25)
     return True
+
 
 def moveCharacter(key, duration, action="walking"):
     pt.keyDown(key)
 
     if action == "walking":
-        print("walking")
         sleep(duration)
     elif action == "attacking":
-        print("attacking")
         pt.keyDown(actions["attacking"])
         sleep(duration)
         pt.keyUp(actions["attacking"])
     
     pt.keyUp(key)
 
-def locateLava():
-    pos = pt.locateCenterOnScreen("lava.png", confidence=0.35)
 
-    if pos is None:
-        print("Could not find lava")
+def lavaFound():
+    try:
+        pos = pt.locateCenterOnScreen("images/lava.png", confidence=0.4)
+    except:
         return False
-
-    pt.keyDown(actions["walkBackward"])
-    sleep(2)
-    pt.keyUp(actions["walkBackward"])
-    return pos
+    
+    return True
 
 
 def main():
-    sleep(5)
-    navToImage("start.png", 1)
+    # Wait for the game to load -> continously check for the "back to game" button
+    while not readyToStart():
+        sleep(1)
 
+    # Main loop -> walk forward and mine until lava is found, then walk backwards
     while True:
-        if locateLava():
+        if lavaFound():
             moveCharacter(actions["walkBackward"], 2)
             break
 
-        moveCharacter(actions["walkForward"], 2)
-        moveCharacter(actions["mine"], 2)
+        moveCharacter(actions["walkForward"], 2, "attacking") # Walks forward and attacks (mines) for 2 seconds
 
 
 if __name__ == "__main__":
